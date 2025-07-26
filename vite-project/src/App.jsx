1f97a98e0ac0d02c32d10c0b6728b571c4daf832
Nikhilesh sahu <nikhileshmkgi30@gmail.com>
2025-07-26 17:05:37 +0530

    im going to learn react and will document all the things here

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Chai from './chai.JSx'
+
+function App() {
+  return (
+    <> //fragement concept
+      <Chai />
+      <h1>chai aur react</h1>
+      <p> this si me Lorem ipsum dolor sit amet.</p>
+    </>
+  )
+}
+
+export default App
