008c6ed6c179a2303740ac7daa9a4895655463ac
Nikhilesh sahu <nikhileshmkgi30@gmail.com>
2025-07-26 20:17:19 +0530

    in this chapter i learn about the hooks concept like why reACT IMPORTANT HOW SIMPLE THE valu update throughout the ui

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //hooks used for to upadate the value in the ui
+
+  let [counter, setCounter] = useState(15)
+  // let counter = 15
+
+  const addValue = () => {
+
+    console.log("clicked", counter);
+    counter = counter + 1
+    setCounter(counter)
+  }
+  const removevalue = () => {
+    // counter = counter - 1
+    setCounter(counter - 1)
+    console.log("clicked", counter);
+  }
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter value:{counter}</h2>
+
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removevalue}>Decrease value {counter}</button>
+      <footer>
+        footer: {counter}
+      </footer>
+    </>
+  )
+}
+
+export default App
